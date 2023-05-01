a4e58b2178916de98d08e21ea8ec7e9a874d4672
David Hunsicker <david.s.hunsicker@gmail.com>
2023-05-01 13:19:08 -0700

    added simple hello world controller and tested.

---
+++ src/main/java/com/example/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
