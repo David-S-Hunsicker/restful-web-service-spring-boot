526ac6b636f1a5bde58fa070776223921fe43b0c
David Hunsicker <david.s.hunsicker@gmail.com>
2023-05-02 16:23:28 -0700

    Added support for Hypermedia versioning of the API

--- src/main/java/com/example/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -16,12 +16,12 @@ public class VersioningPersonController {
     }
 
     @GetMapping(path = "/person", params = "version=1")
-    public PersonV1 getFirstVersionOfPersonRequestParamter() {
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
         return new PersonV1("Tim Apple");
     }
 
     @GetMapping(path = "/person", params = "version=2")
-    public PersonV2 getSecondVersionOfPersonRequestParamter() {
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
         return new PersonV2("Tim", "Apple");
     }
 
@@ -32,6 +32,16 @@ public class VersioningPersonController {
 
     @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
     public PersonV2 getSecondVersionOfPersonRequestHeader() {
+        return new PersonV2("Tim", "Apple");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Tim Apple");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
         return new PersonV2("Tim", "Apple");
     }
 
