f3ed841e7a9227498d1082b73e4bd8e85ac4d54a
David Hunsicker <david.s.hunsicker@gmail.com>
2023-05-02 16:52:58 -0700

    Formatting

--- src/main/java/com/example/rest/webservices/restfulwebservices/users/UserNotFoundException.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/users/UserNotFoundException.java
@@ -2,6 +2,7 @@ package com.example.rest.webservices.restfulwebservices.users;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
