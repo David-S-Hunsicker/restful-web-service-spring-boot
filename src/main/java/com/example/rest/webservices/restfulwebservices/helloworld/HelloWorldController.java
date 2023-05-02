c02b1f395f018334145902c67cad813a582399c0
David Hunsicker <david.s.hunsicker@gmail.com>
2023-05-02 15:37:34 -0700

    Added internationalization / localization to string message. Added messages.properties and bundle for other languages.

--- src/main/java/com/example/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,20 @@
 package com.example.rest.webservices.restfulwebservices.helloworld;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -18,5 +29,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping("/hello-world/internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
