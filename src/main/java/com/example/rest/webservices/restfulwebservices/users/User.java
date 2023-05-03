9038a5ec264b802e367af0b33fee691d952d4da1
David Hunsicker <david.s.hunsicker@gmail.com>
2023-05-03 08:25:34 -0700

    Introduced posts in Post.java and created a relationship to User.java

--- src/main/java/com/example/rest/webservices/restfulwebservices/users/User.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/users/User.java
@@ -1,12 +1,15 @@
 package com.example.rest.webservices.restfulwebservices.users;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity(name = "user_details")
 public class User {
@@ -20,6 +23,10 @@ public class User {
     @Id
     @GeneratedValue
     private Integer id;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
 
     protected User() {
     }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,13 @@ insert into user_details(id,birth_date,name)
 values(1005, current_date(), 'Sakisha');
 insert into user_details(id,birth_date,name)
 values(1003, current_date(), 'Burt');
+
+
+insert into post(id, description, user_id)
+values(2001, 'Im a Scientist', 1001);
+
+insert into post(id, description, user_id)
+values(2002, 'Im a Doctor', 1005);
+
+insert into post(id, description, user_id)
+values(2003, 'I Make lip balm', 1003);
