062f42d44f0eb44ffb09a2ae803a2f34f18652f5
David Hunsicker <david.s.hunsicker@gmail.com>
2023-05-02 18:05:12 -0700

    Modified JsonProperty for User for cleaner reading of JSON on User.java

--- src/main/java/com/example/rest/webservices/restfulwebservices/users/User.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/users/User.java
@@ -1,5 +1,6 @@
 package com.example.rest.webservices.restfulwebservices.users;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -8,8 +9,10 @@ import java.time.LocalDate;
 public class User {
 
     @Past(message = "Birth Date should be in the past.")
+    @JsonProperty("birth_date")
     private final LocalDate birthDate;
     @Size(min = 2, message = "Name should have at least 2 characters.")
+    @JsonProperty("user_name")
     private final String name;
     private Integer id;
 
